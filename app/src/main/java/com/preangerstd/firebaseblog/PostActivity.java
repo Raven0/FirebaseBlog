01346060bc9acd052474e5d8199bd8dcc3801abd
Ismail Rohaga <azh.zhafir@gmail.com>
2017-08-24 17:52:56 +0700

    image on button
    belum di crop(optional)

--- app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
+++ app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
@@ -1,13 +1,40 @@
 package com.preangerstd.firebaseblog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton btnImg;
+    private static final int GALLERY_REQ = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        btnImg = (ImageButton) findViewById(R.id.btnImage);
+
+        btnImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, GALLERY_REQ);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQ && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            btnImg.setImageURI(imageUri);
+        }
     }
 }
