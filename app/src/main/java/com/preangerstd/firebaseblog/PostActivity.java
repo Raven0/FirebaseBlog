30542190c934dd42eed43b73090c609937d8d2a0
Ismail Rohaga <azh.zhafir@gmail.com>
2017-08-24 18:46:16 +0700

    wip upload
    sambil nunggu loading

--- app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
+++ app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
@@ -4,20 +4,35 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton btnImg;
+    private Button btnSubmit;
     private static final int GALLERY_REQ = 1;
+    private Uri imageUri = null;
+    private EditText tbTitle, tbContent;
+    private StorageReference storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        storage = FirebaseStorage.getInstance().getReference();
+
         btnImg = (ImageButton) findViewById(R.id.btnImage);
+        tbTitle = (EditText) findViewById(R.id.tbTitle);
+        tbContent = (EditText) findViewById(R.id.tbContent);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +42,22 @@ public class PostActivity extends AppCompatActivity {
                 startActivityForResult(gallery, GALLERY_REQ);
             }
         });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postNow();
+            }
+        });
+    }
+
+    private void postNow(){
+        String vTitle = tbTitle.getText().toString().trim();
+        String vContent = tbContent.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(vTitle) && !TextUtils.isEmpty(vContent) && imageUri != null){
+
+        }
     }
 
     @Override
