6c369be5637ae54c093fe01cd7610f70a9c59b79
Ismail Rohaga <azh.zhafir@gmail.com>
2017-08-28 10:04:41 +0700

    Alhamdulillah
    ternyata variabel URL

--- app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
+++ app/src/main/java/com/preangerstd/firebaseblog/PostActivity.java
@@ -1,5 +1,6 @@
 package com.preangerstd.firebaseblog;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,17 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton btnImg;
-    private Button btnSubmit;
-    private static final int GALLERY_REQ = 1;
-    private Uri imageUri = null;
     private EditText tbTitle, tbContent;
+    private Button btnSubmit;
+
+    private static final int GALLERY_REQ = 1;
     private StorageReference storage;
+    private Uri imageUri = null;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +33,11 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         storage = FirebaseStorage.getInstance().getReference();
-
         btnImg = (ImageButton) findViewById(R.id.btnImage);
         tbTitle = (EditText) findViewById(R.id.tbTitle);
         tbContent = (EditText) findViewById(R.id.tbContent);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        progressDialog = new ProgressDialog(this);
 
         btnImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,17 +51,27 @@ public class PostActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                postNow();
+                startPosting();
             }
         });
     }
 
-    private void postNow(){
-        String vTitle = tbTitle.getText().toString().trim();
-        String vContent = tbContent.getText().toString().trim();
+    private void startPosting() {
+        progressDialog.setMessage("Uploading");
+        progressDialog.show();
+        String title_val = tbTitle.getText().toString().trim();
+        String desc_val = tbContent.getText().toString().trim();
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && imageUri != null){
+            StorageReference filepath = storage.child("Images").child(imageUri.getLastPathSegment());
 
-        if(!TextUtils.isEmpty(vTitle) && !TextUtils.isEmpty(vContent) && imageUri != null){
-
+            filepath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    @SuppressWarnings("VisibleForTests")
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    progressDialog.dismiss();
+                }
+            });
         }
     }
 
@@ -64,7 +79,7 @@ public class PostActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == GALLERY_REQ && resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             btnImg.setImageURI(imageUri);
         }
     }
