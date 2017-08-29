85062e5baf25106d63cc363e0c2e092209b497a0
Ismail Rohaga <azh.zhafir@gmail.com>
2017-08-29 08:47:53 +0700

    ImageCrop
    setup activity, image crop

--- app/src/main/java/com/preangerstd/firebaseblog/SetupActivity.java
+++ app/src/main/java/com/preangerstd/firebaseblog/SetupActivity.java
@@ -1,13 +1,74 @@
 package com.preangerstd.firebaseblog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private ImageButton mSetupImg;
+    private EditText tbName;
+    private Button btnSetupSubmit;
+
+    private static final int GALLERY_REQ = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        mSetupImg = (ImageButton) findViewById(R.id.btnSetupImg);
+        tbName = (EditText) findViewById(R.id.tbSetupName);
+        btnSetupSubmit = (Button) findViewById(R.id.btnSetupSubmit);
+
+        mSetupImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQ);
+            }
+        });
+
+        btnSetupSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQ && resultCode == RESULT_OK){
+
+            Uri ImageUri = data.getData();
+
+            CropImage.activity(ImageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                mSetupImg.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
